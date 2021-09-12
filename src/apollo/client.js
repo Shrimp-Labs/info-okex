c8d96263ae96e768297f10ec2fb02a40a2acf843
watermelonBe <watergua@protonmail.com>
2021-09-12 21:03:36 +0800

    modify address and text

---
+++ src/apollo/client.js
@@ -0,0 +1,42 @@
+import { ApolloClient } from 'apollo-client'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+
+export const client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://n21.hg.network/subgraphs/name/pipiokex/oec',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
+
+export const healthClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://h21.hg.network/graphql',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
+
+export const v1Client = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://n21.hg.network/subgraphs/name/pipiokex/oec',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
+
+export const stakingClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://n21.hg.network/subgraphs/name/pipiokex/oec',
+  }),
+  cache: new InMemoryCache(),
+  shouldBatch: true,
+})
+
+export const blockClient = new ApolloClient({
+  link: new HttpLink({
+    uri: 'https://n21.hg.network/subgraphs/name/oec-full-blocks/oec',
+  }),
+  cache: new InMemoryCache(),
+})
